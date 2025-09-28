3c7cd212877560f68ae4dc44c956d71e1bde9820
nlizmi <suburbanflats@gmail.com>
2025-09-28 18:54:24 -0400

    progress on part 3

--- part3/main.cpp
+++ part3/main.cpp
@@ -46,12 +46,12 @@ int main(int argc, char* argv[]) {
     }
 
 
-    // Convert JSON array to a dict mapping id -> element
-    std::unordered_map<int, json> dict;
-    for (auto &elem : passages_json) {
-        int id = elem["id"].get<int>();
-        dict[id] = elem;
-    }
+    // // Convert JSON array to a dict mapping id -> element
+    // std::unordered_map<int, json> dict;
+    // for (auto &elem : passages_json) {
+    //     int id = elem["id"].get<int>();
+    //     dict[id] = elem;
+    // }
 
 
     // Parse K and eps
@@ -60,7 +60,7 @@ int main(int argc, char* argv[]) {
 
     try{
         // Extract the query embedding
-        auto query_obj   = query_json[0];
+        auto& query_obj   = query_json[0];
         size_t D         = query_obj["embedding"].size();
         alglib::real_1d_array query;
         query.setlength(D);
@@ -76,12 +76,48 @@ int main(int argc, char* argv[]) {
             - Get the index of each found neighbour  using alglib::kdtreequeryresultstags
             - Get the distance between each found neighbour and the query embedding using alglib::kdtreequeryresultsdists
         */
-        
+        size_t num_embeddings = passages_json.size();
+        alglib::real_2d_array embeddings;
+        alglib::integer_1d_array ids;
+        embeddings.setlength(num_embeddings, D);
+        ids.setlength(num_embeddings);
+
+        for (size_t e = 0; e < num_embeddings; ++e) {
+            auto& embedding_obj = passages_json[e];
+            for (size_t d = 0; d < D; ++d) {
+                embeddings[e][d] = embedding_obj["embedding"][d].get<double>();
+            }
+            ids[e] = embedding_obj["id"].get<alglib::ae_int_t>();
+        }
+        auto processing_end = std::chrono::high_resolution_clock::now();
+
+        auto tree_start = std::chrono::high_resolution_clock::now();
+        alglib::kdtree tree;
+        alglib::kdtreebuildtagged(embeddings, ids, num_embeddings, D, 0, 2, tree);
+        auto tree_end = std::chrono::high_resolution_clock::now();
+
+        auto knn_start = std::chrono::high_resolution_clock::now();
+        alglib::ae_int_t num_neighbors_found = alglib::kdtreequeryaknn(tree, query, k, eps);
+        alglib::real_1d_array found_dists;
+        alglib::integer_1d_array found_ids;
+        found_dists.setlength(num_neighbors_found);
+        found_ids.setlength(num_neighbors_found);
+        alglib::kdtreequeryresultsdistances(tree, found_dists);
+        alglib::kdtreequeryresultstags(tree, found_ids);
+        auto knn_end = std::chrono::high_resolution_clock::now();
+
+        for (alglib::ae_int_t i = 0; i < num_neighbors_found; ++i) {
+            std::cout << "Neighbor " << i+1 << ": distance = " << found_dists[i] << "; id = " << found_ids[i] << std::endl;
+        }
     }
     catch(alglib::ap_error &e) {
         std::cerr << "ALGLIB error: " << e.msg << std::endl;
         return 1;
     }
+
+    auto program_end = std::chrono::high_resolution_clock::now();
+
+    std::cout << program_end - program_start << std::endl;
 
     return 0;
 }
